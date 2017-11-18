37942b12685ab2f6081f62be4681e861d7f62f0c
ansid88 <anna.sidz88@gmail.com>
2017-11-18 20:53:13 +0100

    update

---
+++ src/main/java/com/infoshareacademy/searchengine/servlets/FindUserByLoginServlet.java
@@ -0,0 +1,18 @@
+package com.infoshareacademy.searchengine.servlets;
+import com.infoshareacademy.searchengine.dao.UsersRepositoryDao;
+import com.infoshareacademy.searchengine.dao.UsersRepositoryDaoBean;
+import com.infoshareacademy.searchengine.repository.UsersRepository;
+import com.infoshareacademy.searchengine.domain.User;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/find-user-by-login")
+public class FindUserByLoginServlet extends HttpServlet {
+}
